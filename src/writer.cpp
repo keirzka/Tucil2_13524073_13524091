ab715481798289559bb118ea74b97a07d5d8f1f5
vara <vara.azzara@gmail.com>
2026-03-28 19:33:37 +0700

    fix: winding order counter clock wise

--- src/writer.cpp
+++ src/writer.cpp
@@ -35,14 +35,28 @@ std::vector<vec3> generateCubeVertices(const AABB &box)
 // Membuat 12 face triangle dari kubus
 std::vector<std::array<int, 3>> generateCubeFaces(int offset)
 {
+    int v1 = offset + 1, v2 = offset + 2, v3 = offset + 3, v4 = offset + 4;
+    int v5 = offset + 5, v6 = offset + 6, v7 = offset + 7, v8 = offset + 8;
+
     return {
-        {offset+1, offset+2, offset+3}, {offset+2, offset+4, offset+3},
-        {offset+5, offset+6, offset+7}, {offset+6, offset+8, offset+7},
-        {offset+1, offset+2, offset+5}, {offset+2, offset+6, offset+5},
-        {offset+2, offset+4, offset+6}, {offset+4, offset+8, offset+6},
-        {offset+4, offset+3, offset+8}, {offset+3, offset+7, offset+8},
-        {offset+3, offset+1, offset+7}, {offset+1, offset+5, offset+7}
-    };
+        // Face Bawah (-Z)
+        {v1, v3, v2},
+        {v2, v3, v4},
+        // Face Atas (+Z)
+        {v5, v6, v7},
+        {v6, v8, v7},
+        // Face Kiri (-X)
+        {v1, v5, v3},
+        {v3, v5, v7},
+        // Face Kanan (+X)
+        {v2, v4, v6},
+        {v4, v8, v6},
+        // Face Depan (-Y)
+        {v1, v2, v5},
+        {v2, v6, v5},
+        // Face Belakang (+Y)
+        {v3, v7, v4},
+        {v4, v7, v8}};
 }
 
 // Write to file objek
